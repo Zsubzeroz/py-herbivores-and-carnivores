64501c9ff43a9dc864b13e1740ca277e55cfe5a8
LUAN ESTIFER RODRIGUES PEREIRA <luanestiferjob@gmail.com>
2025-12-05 20:05:22 -0300

    Solution

--- app/main.py
+++ app/main.py
@@ -1,23 +1,25 @@
-class Animal:
-    alive = []
+from typing import List
 
-    def __init__(self, name):
+class Animal:
+    alive: List["Animal"] = []
+
+    def __init__(self, name: str) -> None:
         self.name = name
         self.health = 100
         self.hidden = False
         Animal.alive.append(self)
 
-    def __repr__(self):
+    def __repr__(self) -> str:
         return f"{{Name: {self.name}, Health: {self.health}, Hidden: {self.hidden}}}"
 
 class Herbivore(Animal):
-    def hide(self):
+    def hide(self) -> None:
         self.hidden = not self.hidden
 
 class Carnivore(Animal):
-    def bite(self, target):
-        if isinstance(target, Herbivore) and not target.hidden:
+    def bite(self, target: Animal) -> None:
+        # Verifica se é Herbivoro, se não está escondido E se ainda está vivo
+        if isinstance(target, Herbivore) and not target.hidden and target in Animal.alive:
             target.health -= 50
             if target.health <= 0:
-                if target in Animal.alive:
-                    Animal.alive.remove(target)
+                Animal.alive.remove(target)
